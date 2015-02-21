7a2f30a6e6a781dfac1743c6e56efce3e34300c2
Michael Schnyder <michael.schnyder@zuehlke.com>
2015-02-21 13:23:47 +0100

    added wpf project

---
+++ source/DotNetBay.WPF/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+using DotNetBay.Data.FileStorage;
+using DotNetBay.Interfaces;
+
+namespace DotNetBay.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            this.MainRepository = new FileSystemMainRepository("appdata.json");
+            this.MainRepository.SaveChanges();
+        }
+
+        public IMainRepository MainRepository { get; private set; }
+    }
+}
