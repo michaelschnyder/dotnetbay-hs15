e5cfc6395576519b1e67e938a133c3e81a4e46a4
Michael Schnyder <michael.schnyder@zuehlke.com>
2015-02-21 18:07:20 +0100

    shell viewmodel wip

--- source/DotNetBay.WPF/SellView.xaml.cs
+++ source/DotNetBay.WPF/SellView.xaml.cs
@@ -1,27 +1,91 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+
+using DotNetBay.Core;
+using DotNetBay.Model;
+
+using Microsoft.Win32;
 
 namespace DotNetBay.WPF
 {
     /// <summary>
     /// Interaction logic for SellView.xaml
     /// </summary>
-    public partial class SellView : Window
+    public partial class SellView : Window, INotifyPropertyChanged
     {
+        private readonly AuctionService auctionService;
+
+        private readonly Auction newAuction;
+
+        public Auction NewAuction
+        {
+            get
+            {
+                return newAuction;
+            }
+        }
+
+        public string FilePathToImage { get; set; }
+
         public SellView()
         {
             InitializeComponent();
+
+            this.DataContext = this;
+
+            var app = Application.Current as App;
+
+            var simpleMemberService = new SimpleMemberService(app.MainRepository);
+            this.auctionService = new AuctionService(app.MainRepository, simpleMemberService);
+
+            this.newAuction = new Auction()
+                                  {
+                                      Seller = simpleMemberService.GetCurrentMember(),
+                                      StartDateTimeUtc = DateTime.UtcNow,
+                                      EndDateTimeUtc = DateTime.UtcNow.AddDays(7)
+                                  };
+        }
+
+        private void AddAuction_Click(object sender, RoutedEventArgs e)
+        {
+            this.auctionService.Save(this.newAuction);
+
+            this.Close();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SelectImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == true && File.Exists(openFileDialog.FileName))
+            {
+                var fileInfo = new FileInfo(openFileDialog.FileName);
+                if (fileInfo.Extension.EndsWith("jpg"))
+                {
+                    this.FilePathToImage = openFileDialog.FileName;
+                    this.newAuction.Image = File.ReadAllBytes(this.FilePathToImage);
+                    this.OnPropertyChanged("FilePathToImage");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
         }
     }
 }
