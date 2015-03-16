165c1d03e6d1dd83c5db54b04d8ec0507768b17b
Michael Schnyder <michael.schnyder@zuehlke.com>
2015-03-16 18:37:50 +0100

    startup code

--- source/DotNetBay.WebApp/Global.asax.cs
+++ source/DotNetBay.WebApp/Global.asax.cs
@@ -1,18 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
+using DotNetBay.Core;
+using DotNetBay.Core.Execution;
+using DotNetBay.Data.EF;
+using DotNetBay.Interfaces;
+
 namespace DotNetBay.WebApp
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
+        public static IAuctionRunner AuctionRunner { get; private set; }
+
         protected void Application_Start()
         {
+            // MVC related startup
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            // DotNetBay startup
+            var mainRepository = new EFMainRepository();
+            mainRepository.SaveChanges();
+
+            AuctionRunner = new AuctionRunner(mainRepository);
+            AuctionRunner.Start();
         }
     }
 }
