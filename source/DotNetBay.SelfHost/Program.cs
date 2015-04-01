193dfd22a186539ae009550522140221d6b3cb8b
Michael Schnyder <michael.schnyder@zuehlke.com>
2015-04-01 08:46:03 +0200

    Initial WebApi Commit

---
+++ source/DotNetBay.SelfHost/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using DotNetBay.WebApi.Controller;
+
+using Microsoft.Owin.Hosting;
+
+namespace DotNetBay.SelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var typesLoaded = typeof(StatusController);
+
+            WebApp.Start<Startup>(url: "http://localhost:9001/");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ source/DotNetBay.SelfHost/Startup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(DotNetBay.SelfHost.Startup))]
+
+namespace DotNetBay.SelfHost
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+
+            // Configure Web API for self-host. 
+            HttpConfiguration config = new HttpConfiguration();
+
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            app.UseWebApi(config); 
+        }
+    }
+}
