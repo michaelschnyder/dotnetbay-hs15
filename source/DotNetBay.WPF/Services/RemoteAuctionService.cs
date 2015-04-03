54f65777ffd906176ac18bbfb6fef1ed09ff5de7
Michael Schnyder <michael.schnyder@zuehlke.com>
2015-04-03 17:08:00 +0200

    added remote auction service

---
+++ source/DotNetBay.WPF/Services/RemoteAuctionService.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+using DotNetBay.Core;
+using DotNetBay.Model;
+
+namespace DotNetBay.WPF.Services
+{
+    public class RemoteAuctionService : IAuctionService
+    {
+        public IQueryable<Auction> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Auction Save(Auction auction)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Bid PlaceBid(Auction auction, double amount)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
