6286277b8b7cc3bdbe5e4658b8311db3ffc759d3
Michael Schnyder <michael.schnyder@zuehlke.com>
2015-04-03 15:45:45 +0200

    integrated Web API via OWIN to existing MVC Web Application

---
+++ source/DotNetBay.WebApp/Startup.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Http;
+
+using DotNetBay.WebApp;
+
+using Microsoft.Owin;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace DotNetBay.WebApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Configure Web API for self-host. 
+            var config = new HttpConfiguration();
+
+            config.MapHttpAttributeRoutes();
+
+            config.Formatters.JsonFormatter.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            app.UseWebApi(config);
+            
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
